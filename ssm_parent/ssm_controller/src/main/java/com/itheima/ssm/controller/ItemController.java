fc0f6a9bcd2d9a5494c6b17f22ed192b775fc036
wmin <wmin1202@163.com>
2021-04-26 18:08:18 +0800

    update

---
+++ ssm_parent/ssm_controller/src/main/java/com/itheima/ssm/controller/ItemController.java
@@ -0,0 +1,25 @@
+package com.itheima.ssm.controller;
+
+
+import com.itheima.ssm.pojo.Item;
+import com.itheima.ssm.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/item")
+public class ItemController {
+    @Autowired
+    private ItemService itemService;
+
+    @RequestMapping("/showItem/{id}")   //把访问时的id取出，传给方法
+    public String findById(@PathVariable("id") int id , Model model){
+        Item item = itemService.findById(id);
+        model.addAttribute("item", item);
+        return "item";
+    }
+}
---
+++ ssm_parent/ssm_controller/src/main/webapp/WEB-INF/jsp/item.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2021/4/24
+  Time: 15:00
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" isELIgnored="false" language="java" %>
+<html>
+<head>
+    <title>item展示</title>
+</head>
+<body>
+    <h1>${item}</h1>
+</body>
+</html>
